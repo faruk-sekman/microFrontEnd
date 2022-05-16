abaf9b1e65787f265f20d2b3ec61e229ab3036cf
Faruk Sekman <faruk.sekman@n11.com>
2022-05-16 05:51:10 +0300

    fs

--- projects/store/src/app/products/products.component.ts
+++ projects/store/src/app/products/products.component.ts
@@ -2,17 +2,22 @@ import {Component, OnInit} from '@angular/core';
 
 import {ProductsService} from "./products.service";
 import {ProductsModel} from "./products.model";
+import { trigger } from '@angular/animations';
+import { fadeIn } from './animation';
 
 @Component({
     selector: 'app-products',
     templateUrl: './products.component.html',
-    styleUrls: ['./products.component.scss']
+    styleUrls: ['./products.component.scss'],
+    animations : [
+        trigger('fadeIn', fadeIn())
+    ],
 })
 export class ProductsComponent implements OnInit {
     public products: ProductsModel[] = [];
     public itemCount: number = 6;
     public isComplatedData: boolean = false;
-
+    public dataLoaded: boolean = false;
     constructor(private productsService: ProductsService) {
     }
 
@@ -25,14 +30,21 @@ export class ProductsComponent implements OnInit {
             this.products = data;
             this.isComplatedData = data.length < this.itemCount ? true : false;
             this.itemCount = this.itemCount + 6;
+            this.dataLoaded = true;
         });
     }
 
     getMorePrducts() {
+        this.dataLoaded = false;
         this.productsService.getProducts(this.itemCount).subscribe(data => {
             this.products = data;
             this.isComplatedData = data.length < this.itemCount ? true : false;
             this.itemCount = this.itemCount + 6;
+            this.dataLoaded = true;
         });
+    }
+
+    counter(i: number) {
+        return new Array(i);
     }
 }
